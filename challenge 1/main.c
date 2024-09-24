ecbc52efd6d7530d47132a275178135bf7072e71
anouar el barry <elbarryanouar37@gmail.com>
2024-09-24 16:52:57 +0100

    first commit

--- challenge 1/main.c
+++ challenge 1/main.c
@@ -1,8 +1,15 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+char nome[]="anouar",prenome[]="el barry",sex[]="homme",adress[]="elbarryanouar@gmail.com";
+int age=19;
+printf("le nome:%s \n",nome);
+printf("le prenom:%s \n",prenome);
+printf("le sex:%s \n",sex);
+printf("age:%d \n",age);
+
     return 0;
 }
